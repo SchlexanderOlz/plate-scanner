03dde0f9def1387b9bc0e261dbb962052d53aa32
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2024-06-17 10:05:01 +0200

    Added contour finding of larger white areas

--- crop.py
+++ crop.py
@@ -1,5 +1,31 @@
 import cv2 
 
 def crop(image: cv2.typing.MatLike):
-    blured: cv2.typing.MatLike = cv2.GaussianBlur(image, (5, 5), 0)
-    return image
+    structure = cv2.getStructuringElement( 
+                              shape = cv2.MORPH_RECT, ksize =(22, 3)) 
+
+    grayed = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blured = cv2.GaussianBlur(grayed, (5, 5), 0)
+
+    sobelx = cv2.Sobel(blured, cv2.CV_8U, 1, 0, ksize = 3)  
+        
+    ret2, threshold_img = cv2.threshold(sobelx, 0, 255, 
+                        cv2.THRESH_BINARY + cv2.THRESH_OTSU) 
+
+    element = structure
+    threshhold_copy = threshold_img.copy() 
+    cv2.morphologyEx(src = threshold_img, 
+                        op = cv2.MORPH_CLOSE, 
+                        kernel = element, 
+                        dst = threshhold_copy) 
+
+
+    # Getting contours
+
+    contours, _ = cv2.findContours(threshhold_copy, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    countoured_image = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    cv2.imshow("countoured_image", countoured_image)
+
+
+    return grayed 
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import cv2
 from crop import crop
+import imutils
 
 if __name__ == "__main__":
-    image: cv2.typing.MatLike = cv2.imread("data/license_plates/3375_1389548347.jpg", cv2.IMREAD_GRAYSCALE)
+    image: cv2.typing.MatLike = cv2.imread("data/license_plates/3375_1389548347.jpg", cv2.IMREAD_COLOR)
 
-    cv2.imshow("default", image)
+    image = imutils.resize(image, width=500)
     croped = crop(image)
     cv2.imshow("croped", croped)
 
+
+    while cv2.waitKey(0) != 27:
+        pass
+    cv2.destroyAllWindows()
